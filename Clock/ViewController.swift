7ba1c596da66c7fde8692682112409649467088b
Francisco Soares <chicaoiskemia@gmail.com>
2018-09-05 12:39:40 -0300

    Images for the top shelf items
    Included in ClockTopShelf/ServiceProvider.swift code for generating an image
    for the presented item.
    
    Cleaning up and fixing things in Clock/ViewController.swift:
    - in particular, adding calls to super for touchesBegan and presses* methods.

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -17,20 +17,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let b : Bool = Bool("true")!
-        print(b)
-        
         let df = DateFormatter()
         df.dateFormat = "HH:mm:ss"
         
         self.timeLabel.text = df.string(from: Date())
-        
-        if(UIApplication.shared.supportsAlternateIcons) {
-            print("supports")
-        }
-        else{
-            print("doesn't support")
-        }
         
         self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
         
@@ -50,6 +40,7 @@ class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("touches began")
         print(touches.first?.force ?? 0.0)
+        super.touchesBegan(touches, with: event)
     }
     
     override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
@@ -71,20 +62,25 @@ class ViewController: UIViewController {
             }
             print(toPrint)
         }
+        
+        super.pressesBegan(presses, with: event)
     }
     
     override func pressesChanged(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
         print("presses changed")
         print(presses.first?.force ?? 0.0)
+        super.pressesChanged(presses, with: event)
     }
     
     override func pressesEnded(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
         print("presses ended")
+        super.pressesEnded(presses, with: event)
     }
     
     // pressesCancelled is usually called when a gesture is identified by a gestureRecognizer, thus turning low-level button press recognition useless.
     override func pressesCancelled(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
         print("presses cancelled")
+        super.pressesCancelled(presses, with: event)
     }
     
 
--- ClockTopShelf/ServiceProvider.swift
+++ ClockTopShelf/ServiceProvider.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Francisco Soares. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import TVServices
 
 class ServiceProvider: NSObject, TVTopShelfProvider {
@@ -28,9 +28,6 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
         let wrapperID = TVContentIdentifier(identifier: "wrapper", container: nil)
         let wrapperItem = TVContentItem(contentIdentifier: wrapperID!)
         
-        
-        
-        
         let date = Date()
         let df = DateFormatter()
         df.dateFormat = "HH'h'mm"
@@ -38,14 +35,60 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
         let tvcItem = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "Teste", container: wrapperID)!)
         tvcItem?.title = stringDate
         tvcItem?.imageShape = .square
-        tvcItem?.setImageURL(nil, forTraits: .screenScale1x)
+        
+        // creating the item's image
+        let view = makeView(from: stringDate)
+        let image = loadImageFrom(view: view)
+        let imageUrl = save(image: image)
+        
+        tvcItem?.setImageURL(imageUrl, forTraits: .screenScale1x)
         
         wrapperItem?.title = stringDate//"Wrapper Item"
         wrapperItem?.topShelfItems = [tvcItem!]
-        
         
         return [wrapperItem!]
     }
 
 }
+
+func loadImageFrom(view: UIView) -> UIImage {
+    let size = view.bounds.size
+    UIGraphicsBeginImageContext(size)
+    let ctx = UIGraphicsGetCurrentContext()!
+    view.layer.render(in: ctx)
+    let img = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    return img
+}
+
+func save(image: UIImage) -> URL? {
+    
+    let url = URL(fileURLWithPath: NSTemporaryDirectory())
+        .appendingPathComponent(UUID().uuidString)
+        .appendingPathExtension("png")
+    
+    if let data = UIImagePNGRepresentation(image) {
+        try? data.write(to: url)
+    }
+    
+    return url
+}
+
+func makeView(from string: String) -> UIView {
+    let view = UIView(frame: CGRect(x:0, y: 0, width: 608, height: 608))
+    view.backgroundColor = .white
+    
+    let lb = UILabel(frame: CGRect(x: 18, y: 304-200, width: 590, height: 400))
+    
+    lb.font = UIFont.boldSystemFont(ofSize: 150)
+    lb.text = string
+    lb.textColor = .black
+    lb.backgroundColor = .clear
+    
+    view.addSubview(lb)
+    return view
+}
+
+
+
 

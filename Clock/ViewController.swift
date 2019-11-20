dc81bd0ac59b9ead01e7955afd8529f65d1c447c
Francisco Soares <fmssn@cin.ufpe.br>
2019-11-20 17:17:24 -0300

    Fixes for working with dark mode and cleaning up code

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -34,54 +34,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: Examples of button pressing functions
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touches began")
-        print(touches.first?.force ?? 0.0)
-        super.touchesBegan(touches, with: event)
-    }
-    
-    override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
-        print("presses began")
-        print(presses.first?.force ?? 0.0)
-        
-        
-        for press in presses {
-            let toPrint : String
-            switch press.type {
-            case .playPause:
-                toPrint = "pressed play"
-            case .select:
-                toPrint = "pressed select"
-            case .upArrow:
-                toPrint = "pressed up"
-            default:
-                toPrint = "another button pressed"
-            }
-            print(toPrint)
-        }
-        
-        super.pressesBegan(presses, with: event)
-    }
-    
-    override func pressesChanged(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
-        print("presses changed")
-        print(presses.first?.force ?? 0.0)
-        super.pressesChanged(presses, with: event)
-    }
-    
-    override func pressesEnded(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
-        print("presses ended")
-        super.pressesEnded(presses, with: event)
-    }
-    
-    // pressesCancelled is usually called when a gesture is identified by a gestureRecognizer, thus turning low-level button press recognition useless.
-    override func pressesCancelled(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
-        print("presses cancelled")
-        super.pressesCancelled(presses, with: event)
-    }
     
 
 }

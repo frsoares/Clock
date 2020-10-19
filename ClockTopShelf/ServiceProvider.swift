a6cf29b4cc61c9515c0942add7362f7a53fbe658
Francisco Soares <fmssn@cin.ufpe.br>
2020-10-18 22:45:44 -0300

    updated to newest swift version

--- ClockTopShelf/ServiceProvider.swift
+++ ClockTopShelf/ServiceProvider.swift
@@ -10,10 +10,10 @@ import UIKit
 import TVServices
 
 class ServiceProvider: NSObject, TVTopShelfProvider {
-
-    override init() {
-        super.init()
-    }
+//
+//    override init() {
+//        super.init()
+//    }
 
     // MARK: - TVTopShelfProvider protocol
 
@@ -26,27 +26,27 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
         // Create an array of TVContentItems.
         
         let wrapperID = TVContentIdentifier(identifier: "wrapper", container: nil)
-        let wrapperItem = TVContentItem(contentIdentifier: wrapperID!)
+        let wrapperItem = TVContentItem(contentIdentifier: wrapperID)
         
         let date = Date()
         let df = DateFormatter()
         df.dateFormat = "HH'h'mm"
         let stringDate = df.string(from: date)
-        let tvcItem = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "Teste", container: wrapperID)!)
-        tvcItem?.title = stringDate
-        tvcItem?.imageShape = .square
+        let tvcItem = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "Teste", container: wrapperID))
+        tvcItem.title = stringDate
+        tvcItem.imageShape = .square
         
         // creating the item's image
         let view = makeView(from: stringDate)
         let image = loadImageFrom(view: view)
         let imageUrl = save(image: image)
         
-        tvcItem?.setImageURL(imageUrl, forTraits: .screenScale1x)
+        tvcItem.setImageURL(imageUrl, forTraits: .screenScale1x)
         
-        wrapperItem?.title = stringDate//"Wrapper Item"
-        wrapperItem?.topShelfItems = [tvcItem!]
+        wrapperItem.title = stringDate//"Wrapper Item"
+        wrapperItem.topShelfItems = [tvcItem]
         
-        return [wrapperItem!]
+        return [wrapperItem]
     }
 
 }
@@ -67,15 +67,19 @@ func save(image: UIImage) -> URL? {
         .appendingPathComponent(UUID().uuidString)
         .appendingPathExtension("png")
     
-    if let data = UIImagePNGRepresentation(image) {
-        try? data.write(to: url)
+    if let data = image.pngData() {
+        do {
+            try data.write(to: url)
+        } catch {
+            print(error)
+        }
     }
     
     return url
 }
 
 func makeView(from string: String) -> UIView {
-    let view = UIView(frame: CGRect(x:0, y: 0, width: 608, height: 608))
+    let view = UIView(frame: CGRect(x:0, y: 0, width: 908, height: 512))//608, height: 608))//1940, height: 692))//608, height: 608))
     view.backgroundColor = .white
     
     let lb = UILabel(frame: CGRect(x: 18, y: 304-200, width: 590, height: 400))
@@ -83,7 +87,7 @@ func makeView(from string: String) -> UIView {
     lb.font = UIFont.boldSystemFont(ofSize: 150)
     lb.text = string
     lb.textColor = .black
-    lb.backgroundColor = .clear
+    lb.backgroundColor = .red
     
     view.addSubview(lb)
     return view
